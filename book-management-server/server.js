1941341fdb8ec70c51357160faa56f1e06ddbb38
koyama <g022c1048@g.neec.ac.jp>
2024-07-22 19:15:50 +0900

    trim

--- book-management-server/server.js
+++ book-management-server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 // MySQLデータベース接続
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
+  host: process.env.MYSQL_HOST.trim(),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
